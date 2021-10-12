747dc409f6ded9005d4704828ea7c3dbcfbf45bd
Muschke <mussche.yannick@hotmail.be>
2021-10-12 12:53:35 +0200

    fragments

--- src/main/java/be/vdab/frituurfrida/controllers/SausController.java
+++ src/main/java/be/vdab/frituurfrida/controllers/SausController.java
@@ -1,10 +1,14 @@
 package be.vdab.frituurfrida.controllers;
 
+import be.vdab.frituurfrida.domain.Adres;
+import be.vdab.frituurfrida.domain.Gemeente;
 import be.vdab.frituurfrida.domain.Saus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("sauzen")
@@ -17,7 +21,10 @@ class SausController {
             new Saus(5, "vinaigrette", new String[] {"ciderazijn", "graanmosterd", "olijfolie"})};
     @GetMapping
     public ModelAndView sauzen() {
-        return new ModelAndView("index", "sauzen", sauzen);
-
+        var maandagOfAndereDag = LocalDate.now().getDayOfWeek().equals("1") ? "Vandaag zijn wij gesloten" : "Wij zijn open, u bent welkom";
+        var modelAndView =  new ModelAndView("sauzen", "dagVdWeek", maandagOfAndereDag);
+        modelAndView.addObject("locatie", new Adres("Krieltjesweg", 101, new Gemeente("Bruhhe", 4880)));
+        modelAndView.addObject("sauzen", sauzen);
+        return modelAndView;
     }
 }
